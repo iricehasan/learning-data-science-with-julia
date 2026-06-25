8391b658918d470aa559f826b46ad1fc8875fdb5
iricehasan <hsn.irice@gmail.com>
2026-06-26 00:24:26 +0300

    feat: add unicodes

--- learning-julia/basics.jl
+++ learning-julia/basics.jl
@@ -10,4 +10,20 @@ Common operations
 
 # instead of 2*x we can write 2x
 x = 5
-print(2x)
+println(2x)
+
+# for the same reason we can use unicode characters
+
+β = 42
+println(β)
+
+# \_ for subscript and \^for superscript
+L₀ = 1
+println(L₀)
+L² = 4
+println(L²)
+
+# some already have assigned values
+# \pi and \euler
+println(π)
+println(ℯ)
