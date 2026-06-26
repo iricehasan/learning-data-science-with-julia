e03292be6e1f637a935c8c42ff0e8f3a5f680b3e
iricehasan <hsn.irice@gmail.com>
2026-06-26 19:48:23 +0300

    feat: add For Loops

---
+++ learning-julia/basics-2.jl
@@ -0,0 +1,12 @@
+# For Loop
+
+# 1:100 defines a range of all the numbers from 1 to 100, with a step of 1
+for i in 1:100
+    println(i)
+end
+
+# it is possible to iterate over arrays
+arr = [1, 3, 2, 2]
+for element in arr
+    println(element)
+end
